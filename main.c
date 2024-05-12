10bec8047ad0129a5103303063f5a934cdda08ce
Xuan Lam Pham <phamxuanlam@gmail.com>
2024-05-12 16:16:25 +0700

    Update main.c

--- main.c
+++ main.c
@@ -1,26 +1,16 @@
 #include <stdio.h>
 
 int main() {
-    int num_terms, first = 0, second = 1, next;
+    int num;
 
     printf("Enter a number: ");
-    scanf("%d", &num_terms);
+    scanf("%d", &num);
 
     printf("Fibonacci sequence: ");
 
     // Generate Fibonacci sequence using a loop
-    for (int i = 0; i < num_terms; ++i) {
-        if (i <= 1)
-            next = i;
-        else {
-            next = first + second;
-            first = second;
-            second = next;
-        }
-        printf("%d ", next);
-    }
 
-    printf("\n");
+    //Your Code
 
     return 0;
 }
